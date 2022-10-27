b0ad4933e46ca7cbb45ca2f4005565c062e8004f
Ciro Buregio <ciro.buregio@propig.com.br>
2022-10-27 17:47:13 -0300

    Add NSU

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/NSU/{nsu}")
+async def read_item(nsu: int):
+    
+    return {"nsu": nsu}
